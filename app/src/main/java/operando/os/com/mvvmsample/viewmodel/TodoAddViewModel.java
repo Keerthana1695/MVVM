0a22d1b519aa2690a94310964ccde4c7db9150fc
operando <okanoshinobu8@gmail.com>
2016-05-03 16:16:53 +0900

    Delete commentout

--- app/src/main/java/operando/os/com/mvvmsample/viewmodel/TodoAddViewModel.java
+++ app/src/main/java/operando/os/com/mvvmsample/viewmodel/TodoAddViewModel.java
@@ -14,21 +14,6 @@ public class TodoAddViewModel extends BaseObservable {
         isEnable = new ObservableBoolean(false);
     }
 
-//    @BindingAdapter("addTextWatcher")
-//    public void addTextWatcher(TextView view) {
-//        RxTextView.afterTextChangeEvents(view).map(new Func1<TextViewAfterTextChangeEvent, Boolean>() {
-//            @Override
-//            public Boolean call(TextViewAfterTextChangeEvent textViewAfterTextChangeEvent) {
-//                return !TextUtils.isEmpty(textViewAfterTextChangeEvent.editable().toString());
-//            }
-//        }).subscribe(new Action1<Boolean>() {
-//            @Override
-//            public void call(Boolean enabled) {
-//                isEnable.set(enabled);
-//            }
-//        });
-//    }
-
     public TextWatcher getTodoEditTextWatcher() {
         return new TextWatcher() {
             @Override
@@ -46,18 +31,4 @@ public class TodoAddViewModel extends BaseObservable {
             }
         };
     }
-
-//    @BindingAdapter("onClick")
-//    public void setOnClick() {
-//
-////        binding.todoSubmit.setOnClickListener(new View.OnClickListener() {
-////            @Override
-////            public void onClick(View v) {
-////                Todo todo = new Todo(binding.todoEdit.getText().toString(), false);
-////                TodoDataRepository.add(todo);
-////                setResult(RESULT_OK);
-////                finish();
-////            }
-////        });
-//    }
 }
